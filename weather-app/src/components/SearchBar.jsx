4bc9bf3a9915fa31d1850b0da9c73f817a477a0f
Luciana <lucianachamorro87@gmail.com>
2022-07-03 21:43:03 -0300

    searcbar logic, fails fetching data

--- weather-app/src/components/SearchBar.jsx
+++ weather-app/src/components/SearchBar.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function SearchBar(props) {
   // acá va tu código
-  const {onSearch} = props;
+  const {search} = props;
   return (
      <div>
   <nav>
     <input type = 'text' placeholder ='City' background = 'green'></input>
-    <button type='submit' onClick={onSearch}>Add</button>
+    <button type='submit' onClick={search}>Add</button>
   </nav>
   </div>
   )
